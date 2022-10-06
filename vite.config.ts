c76a68603b2f9348d9bf95f8aa1c44136763a143
kiiiyo <kiiiyo.jp@gmail.com>
2022-10-06 22:01:54 +0900

    chore: modify config dev tool

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "./src",
   plugins: [react(), vanillaExtractPlugin()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@/": path.join(__dirname, "src/"),
+    },
+  },
 });
