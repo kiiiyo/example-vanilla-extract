543c8e54bf2d2d3b958f521af19aeb6de51bae54
kiiiyo <kiiiyo.jp@gmail.com>
2022-10-16 10:12:13 +0900

    feat: modify SiteHeader component

--- src/components/organisms/site-header/site-header.component.tsx
+++ src/components/organisms/site-header/site-header.component.tsx
@@ -1,5 +1,12 @@
 import * as styles from "./styles.css";
 
 export const SiteHeader = () => {
-  return <header className={styles.headerContainer}>SiteHeader</header>;
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.siteBrand}>
+        <div className={styles.siteBrandIcon}>🌲</div>
+        <div className={styles.siteBrandName}>SiteName</div>
+      </div>
+    </header>
+  );
 };
--- src/components/organisms/site-header/styles.css.ts
+++ src/components/organisms/site-header/styles.css.ts
@@ -2,4 +2,22 @@ import { style } from "@vanilla-extract/css";
 
 export const headerContainer = style({
   display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  minHeight: "4rem",
+  borderBottom: "1px solid #d2d3d7", // TODO
+  padding: "0 2rem",
+});
+
+export const siteBrand = style({
+  display: "flex",
+});
+
+export const siteBrandIcon = style({
+  marginRight: "0.5rem",
+  fontSize: "1.5rem",
+});
+
+export const siteBrandName = style({
+  fontSize: "1.5rem",
 });
