2bde7f2104f81855e7cdd4b9ff27a30c66e6a0cf
kiiiyo <kiiiyo.jp@gmail.com>
2022-10-26 14:12:58 +0900

    feat: footer style

--- src/components/organisms/site-footer/site-footer.component.tsx
+++ src/components/organisms/site-footer/site-footer.component.tsx
@@ -2,72 +2,80 @@ import * as styles from "./styles.css";
 
 export const SiteFooter = () => {
   return (
-    <footer className={styles.footer}>
-      <nav role="navigation" className={styles.container}>
-        <div className={styles.footerGrid}>
-          <div className={styles.footerBrand}>
-            <div className={styles.footerBrandIcon}>🌲</div>
-            <div className={styles.footerBrandName}>SiteName</div>
+    <footer className={styles.root}>
+      <div className={styles.container}>
+        <nav role="navigation" className={styles.inner}>
+          <div className={styles.siteContainer}>
+            <div className={styles.brandGroup}>
+              <div className={styles.brandIcon}>🌲</div>
+              <div className={styles.brandName}>Forest UI</div>
+            </div>
+            <p className={styles.description}>
+              TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText
+            </p>
+            <div className={styles.snsGroup}>
+              <button className={styles.snsItem}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  role="img"
+                  className={styles.snsIcon}
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M17 3.5a.5.5 0 0 0-.5-.5H14a4.77 4.77 0 0 0-5 4.5v2.7H6.5a.5.5 0 0 0-.5.5v2.6a.5.5 0 0 0 .5.5H9v6.7a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-6.7h2.62a.5.5 0 0 0 .49-.37l.72-2.6a.5.5 0 0 0-.48-.63H13V7.5a1 1 0 0 1 1-.9h2.5a.5.5 0 0 0 .5-.5Z"
+                  ></path>
+                </svg>
+              </button>
+              <button className={styles.snsItem}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  role="img"
+                  className={styles.snsIcon}
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M8.08 20A11.07 11.07 0 0 0 19.52 9A8.09 8.09 0 0 0 21 6.16a.44.44 0 0 0-.62-.51a1.88 1.88 0 0 1-2.16-.38a3.89 3.89 0 0 0-5.58-.17A4.13 4.13 0 0 0 11.49 9C8.14 9.2 5.84 7.61 4 5.43a.43.43 0 0 0-.75.24a9.68 9.68 0 0 0 4.6 10.05A6.73 6.73 0 0 1 3.38 18a.45.45 0 0 0-.14.84A11 11 0 0 0 8.08 20"
+                  ></path>
+                </svg>
+              </button>
+            </div>
           </div>
-
-          <div className={styles.footerGroup}>
-            <h2 className={styles.footerHeader}>Headline</h2>
-            <ul className={styles.footerList}>
-              <li className={styles.footerListItem}>
-                <a className={styles.footerListAnker}>Link Text</a>
-              </li>
-              <li className={styles.footerListItem}>
-                <a className={styles.footerListAnker}>Link Text</a>
-              </li>
-              <li className={styles.footerListItem}>
-                <a className={styles.footerListAnker}>Link Text</a>
-              </li>
-            </ul>
+          <div className={styles.navigationContainer}>
+            <div className={styles.navigationInner}>
+              <div className={styles.navigationGroup}>
+                <h2 className={styles.navigationTitle}>About</h2>
+                <a className={styles.navigationLink}>About us</a>
+                <a className={styles.navigationLink}>Contact us</a>
+                <a className={styles.navigationLink}>FAQs</a>
+              </div>
+              <div className={styles.navigationGroup}>
+                <h2 className={styles.navigationTitle}>Legal</h2>
+                <a className={styles.navigationLink}>Terms and Condition</a>
+                <a className={styles.navigationLink}>Privacy Policy</a>
+              </div>
+              <div className={styles.navigationGroup}>
+                <h2 className={styles.navigationTitle}>Links</h2>
+                <a className={styles.navigationLink}>Twitter</a>
+                <a className={styles.navigationLink}>GitHub</a>
+                <a className={styles.navigationLink}>Feed</a>
+              </div>
+            </div>
           </div>
-          <div className={styles.footerGroup}>
-            <h2 className={styles.footerHeader}>Headline</h2>
-            <ul className={styles.footerList}>
-              <li className={styles.footerListItem}>
-                <a className={styles.footerListAnker}>Link Text</a>
-              </li>
-              <li className={styles.footerListItem}>
-                <a className={styles.footerListAnker}>Link Text</a>
-              </li>
-              <li className={styles.footerListItem}>
-                <a className={styles.footerListAnker}>Link Text</a>
-              </li>
-            </ul>
+        </nav>
+        <section>
+          <div className={styles.copyright}>
+            <span>Copyright © 2022 All rights reserved.</span>
           </div>
-          <div className={styles.footerGroup}>
-            <h2 className={styles.footerHeader}>Headline</h2>
-            <ul className={styles.footerList}>
-              <li className={styles.footerListItem}>
-                <a className={styles.footerListAnker}>Link Text</a>
-              </li>
-              <li className={styles.footerListItem}>
-                <a className={styles.footerListAnker}>Link Text</a>
-              </li>
-              <li className={styles.footerListItem}>
-                <a className={styles.footerListAnker}>Link Text</a>
-              </li>
-            </ul>
-          </div>
-          <div className={styles.footerGroup}>
-            <h2 className={styles.footerHeader}>Headline</h2>
-            <ul className={styles.footerList}>
-              <li className={styles.footerListItem}>
-                <a className={styles.footerListAnker}>Link Text</a>
-              </li>
-              <li className={styles.footerListItem}>
-                <a className={styles.footerListAnker}>Link Text</a>
-              </li>
-              <li className={styles.footerListItem}>
-                <a className={styles.footerListAnker}>Link Text</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+        </section>
+      </div>
     </footer>
   );
 };
--- src/components/organisms/site-footer/styles.css.ts
+++ src/components/organisms/site-footer/styles.css.ts
@@ -1,72 +1,169 @@
 import { style } from "@vanilla-extract/css";
 
-export const footer = style({
+export const root = style({
   backgroundColor: "fafafa", // TODO
-  borderTop: "1px solid #eaeaea", //TODO
-  padding: "3rem 0 0", // TODO
 });
 
 export const container = style({
   maxWidth: 1200, //TODO: var --geist-page-width
   margin: "0 auto",
-  padding: "0 2rem",
-});
-
-export const footerGrid = style({
-  display: "grid",
-  gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr",
+  paddingTop: 80,
+  paddingLeft: 16,
+  paddingRight: 16,
   "@media": {
-    "screen and (max-width: 768px)": {
-      gridTemplateColumns: "1fr 1fr 1fr 1fr",
+    "screen and (min-width: 768px)": {
+      paddingLeft: 24,
+      paddingRight: 24,
     },
   },
-  columnGap: "3%",
 });
 
-export const footerBrand = style({
+export const inner = style({
   display: "flex",
+  boxSizing: "border-box",
+  flexFlow: "row wrap",
+  width: "100%",
+  justifyContent: "center",
   "@media": {
-    "screen and (max-width: 768px)": {
-      gridColumn: "span 4",
-      marginBottom: "2rem",
+    "screen and (min-width: 900px)": {
+      justifyContent: "space-between",
     },
   },
 });
 
-export const footerBrandIcon = style({
+export const siteContainer = style({
+  flexGrow: 0,
+  flexBasis: "100%",
+  maxWidth: "100%",
+  flexDirection: "row",
+  "@media": {
+    "screen and (min-width: 900px)": {
+      flexBasis: "25%",
+      maxWidth: "25%",
+    },
+  },
+});
+
+export const brandGroup = style({
+  flexGrow: 0,
+  boxSizing: "border-box",
+  margin: "0px 0px 24px",
+  flexDirection: "row",
+  display: "flex",
+  justifyContent: "center",
+  "@media": {
+    "screen and (min-width: 900px)": {
+      justifyContent: "start",
+    },
+  },
+});
+
+export const brandIcon = style({
   marginRight: "0.5rem",
   fontSize: "1.5rem",
 });
 
-export const footerBrandName = style({
+export const brandName = style({
   fontSize: "1.5rem",
 });
 
-export const footerGroup = style({
-  marginBottom: "2rem",
+export const description = style({
+  fontSize: "0.875rem",
+  wordBreak: "break-all",
+  width: "60%",
+  margin: "0 auto 40px",
   "@media": {
-    "screen and (max-width: 425px)": {
-      gridColumn: "span 2",
+    "screen and (min-width: 900px)": {
+      width: "100%",
     },
   },
 });
 
-export const footerHeader = style({
-  fontWeight: "bold",
-  fontSize: ".875rem",
-  margin: "0 0 1rem",
+export const snsGroup = style({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  marginBottom: 40,
+  "@media": {
+    "screen and (min-width: 900px)": {
+      justifyContent: "start",
+    },
+  },
 });
 
-export const footerList = style({
-  listStyle: "none",
-  margin: "0",
-  padding: "0",
+export const snsItem = style({
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  outline: 0,
+  border: 0,
+  margin: 0,
+  cursor: "pointer",
+  appearance: "none",
+  padding: "8px",
+  backgroundColor: "transparent",
 });
 
-export const footerListItem = style({
-  lineHeight: "1.25",
-  fontSize: ".875rem",
-  marginBottom: "1rem",
+export const snsIcon = style({
+  width: 20,
+  height: 20,
 });
 
-export const footerListAnker = style({});
+export const navigationContainer = style({
+  flexBasis: "58.3333%",
+  flexGrow: 0,
+  maxWidth: "58.3333%",
+});
+
+export const navigationInner = style({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  "@media": {
+    "screen and (min-width: 900px)": {
+      flexDirection: "row",
+    },
+  },
+});
+
+export const navigationGroup = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  minWidth: 120,
+  marginBottom: 40,
+  "@media": {
+    "screen and (min-width: 900px)": {
+      alignItems: "flex-start",
+    },
+  },
+});
+
+export const navigationTitle = style({
+  margin: 0,
+  fontWeight: 700,
+  fontSize: "0.75rem",
+  lineHeight: 1.5,
+});
+
+export const navigationLink = style({
+  margin: "1rem 0 0",
+  fontWeight: 400,
+  fontSize: "0.875rem",
+  lineHeight: 1.5,
+  textDecoration: "none",
+});
+
+export const copyright = style({
+  margin: "80px 0px 0px",
+  paddingBottom: 40,
+  fontSize: "0.75rem",
+  lineHeight: 1.5,
+  fontWeight: 400,
+  textAlign: "center",
+  "@media": {
+    "screen and (min-width: 900px)": {
+      textAlign: "left",
+    },
+  },
+});
